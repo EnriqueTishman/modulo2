c19ac1180837c6464dbed4c87030c7ae8a26d45a
EnriqueTC <tishman95@gmail.com>
2023-12-03 11:26:16 -0600

    reorganizar

---
+++ seman1/clase1/script.js
@@ -0,0 +1,53 @@
+let nombre1 = prompt('Hola ¿Cómo te llams?');
+alert('Hola ' + nombre1 + ' Mi Nombre es pc');
+let edad1 = prompt('¿Cuantos años tienes?');
+alert('Tienes ' + edad1 + ' años')
+
+console.log('2+2=' + (2+2));
+console.log('4 * 25684 ='+ (4*25684))
+
+//operador modilo "%" residuo de divicion 
+console.log('324 / 24 =' + (324/24));
+console.log('324 % 24 =' + (324%24));
+
+
+
+
+
+
+let perro = {
+    generales: {
+        nombre: "Arya",
+        edad:2,
+    
+    },
+    informacionNdacimineto: {
+        cuidad:'Veracruz',
+        mesDeNacimiento: 
+        'Julio',
+        diaDeNaciomoento: 16
+
+    }
+}
+
+console.log(perro.generales.nombre);
+console.log(perro.informacionNdacimineto)
+
+const gato ={
+    nombre: 'Logan',
+    edad: 6,
+}
+
+console.log(gato.nombre);
+ console.log(gato);
+ gato.edad 
+ 
+
+ //gato = 10, no se puede hacer por que es modificar la constante 
+
+
+
+
+
+
+

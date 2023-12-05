32be70c61658af877178d2f4d047e580fcc41fe4
EnriqueTC <tishman95@gmail.com>
2023-12-05 08:41:36 -0600

    finalizado

---
+++ seman1/clase2/ejercicio1.js
@@ -0,0 +1,12 @@
+let persona = {
+    nombre: "Patricia",
+    edad: 28,
+    localidad: "Norte",
+    embarazada:  true,
+    semanas: 12
+}
+if(persona.edad >=18 && persona.localidad == "Norte" && persona.embarazada == true && persona.semanas>=9){
+    console.log("Te puedes vacunar");
+}else{
+    console.log("Lo sentimos, todavia no te puedes vacunar");
+}
